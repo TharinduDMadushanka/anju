9c2dc7ee9ee8afc94dd5e71de7a0a32af23c4a82
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-05 11:41:07 +0530

    create setUi method for signup

--- Final Project/src/com/dev/pos/controller/SignupFormController.java
+++ Final Project/src/com/dev/pos/controller/SignupFormController.java
@@ -1,9 +1,14 @@
 package com.dev.pos.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -14,5 +19,12 @@ public class SignupFormController {
     }
 
     public void btnAlreadyHaveOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
     }
 }
