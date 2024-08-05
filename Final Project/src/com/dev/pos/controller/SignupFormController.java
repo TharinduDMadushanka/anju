fba7669cb165a19df88f21764c512b270f4d96a0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-05 11:35:19 +0530

    create signupForm Structure

---
+++ Final Project/src/com/dev/pos/controller/SignupFormController.java
@@ -0,0 +1,18 @@
+package com.dev.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SignupFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAlreadyHaveOnAction(ActionEvent actionEvent) {
+    }
+}
