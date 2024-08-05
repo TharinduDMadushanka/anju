c5813efd34572c2a2b2be9ece0ff80bde4f1acbd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-05 11:29:41 +0530

    create loginForm structure

---
+++ Final Project/src/com/dev/pos/controller/LoginFormController.java
@@ -0,0 +1,18 @@
+package com.dev.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSignupOnAction(ActionEvent actionEvent) {
+    }
+}
