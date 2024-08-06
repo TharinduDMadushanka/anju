c5efdaa36fec4e8c312a10c0731c4fdcc4d82625
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-06 22:46:58 +0530

    complete customer update

--- Final Project/src/com/dev/pos/controller/CustomerFormController.java
+++ Final Project/src/com/dev/pos/controller/CustomerFormController.java
@@ -95,7 +95,14 @@ public class CustomerFormController {
                 }
             }
         }else {
-
+            boolean isUpdated = DatabaseAccessCode.updateCustomer(dto);
+            if (isUpdated) {
+                new Alert(Alert.AlertType.INFORMATION, "Customer has been updated!").show();
+                txtEmail.setEditable(true);
+                btnSave.setText("Save Customer");
+                clearFields();
+                loadCustomer(searchText);
+            }
         }
     }
 
