d8ea43484985b7041218ae2e82ea08417ec8dc7d
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-06 18:00:53 +0530

    create dashboard UI

--- Final Project/src/com/dev/pos/controller/DashboardFormController.java
+++ Final Project/src/com/dev/pos/controller/DashboardFormController.java
@@ -1,4 +1,39 @@
 package com.dev.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+
+    public AnchorPane context;
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
+    }
+
+    public void btnLogoutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomerManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOrderDetailsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnIncomeReportOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnProductManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
+    }
 }
