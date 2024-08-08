77d5c19ce34dab892b5174b58543076a31fef466
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-08 09:30:12 +0530

    complete CustomerDao interface

--- Final Project/src/com/dev/pos/dao/custom/CustomerDao.java
+++ Final Project/src/com/dev/pos/dao/custom/CustomerDao.java
@@ -1,4 +1,16 @@
 package com.dev.pos.dao.custom;
 
+import com.dev.pos.entity.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+
+    public boolean saveCustomer(Customer customer);
+    public boolean updateCustomer(Customer customer);
+    public boolean deleteCustomer(String email);
+    public Customer findCustomer(String email);
+    public List<Customer> findAllCustomers();
+    public List<Customer> searchCustomer(String value);
+
 }
