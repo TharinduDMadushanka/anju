e22481ef120ca86fe2df6b82d32621ce3a248326
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-08 09:33:08 +0530

    complete ProductDao interface

--- Final Project/src/com/dev/pos/dao/custom/ProductDao.java
+++ Final Project/src/com/dev/pos/dao/custom/ProductDao.java
@@ -1,4 +1,16 @@
 package com.dev.pos.dao.custom;
 
+import com.dev.pos.entity.Product;
+
+import java.util.List;
+
 public interface ProductDao {
+
+    public boolean saveProduct(Product product);
+    public boolean updateProduct(Product product);
+    public boolean deleteProduct(int code);
+    public Product findProduct(int code);
+    public List<Product> findAllProduct();
+    public List<Product> searchProduct(String value);
+
 }
