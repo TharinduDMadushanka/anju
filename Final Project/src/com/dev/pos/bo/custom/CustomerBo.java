3f6340f349c8bf5fbcde1a0cb514aeffe2fb2b2e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-18 10:36:51 +0530

    complete CustomerBo interface

--- Final Project/src/com/dev/pos/bo/custom/CustomerBo.java
+++ Final Project/src/com/dev/pos/bo/custom/CustomerBo.java
@@ -1,4 +1,17 @@
 package com.dev.pos.bo.custom;
 
+import com.dev.pos.dto.CustomerDTO;
+import com.dev.pos.entity.Customer;
+
+import java.util.List;
+
 public interface CustomerBo {
+
+    public boolean saveCustomer(CustomerDTO customerDTO)throws Exception;
+    public boolean updateCustomer(CustomerDTO customerDTO)throws Exception;
+    public boolean deleteCustomer(String email)throws Exception;
+    public CustomerDTO findCustomer(String email)throws Exception;
+    public List<CustomerDTO> searchCustomer(String value)throws Exception;
+    public List<CustomerDTO> findAllCustomer()throws Exception;
+
 }
