8c51e35f4cc407d5aae00b38d58c1da7767c81c6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-18 11:00:34 +0530

    complete UserBoImpl class

--- Final Project/src/com/dev/pos/bo/custom/UserBo.java
+++ Final Project/src/com/dev/pos/bo/custom/UserBo.java
@@ -1,4 +1,10 @@
 package com.dev.pos.bo.custom;
 
+import com.dev.pos.dto.UserDTO;
+
 public interface UserBo {
+
+    public boolean saveUser(UserDTO userDTO) throws Exception;
+    public UserDTO findUser(String email) throws Exception;
+
 }
--- Final Project/src/com/dev/pos/bo/impl/UserBoImpl.java
+++ Final Project/src/com/dev/pos/bo/impl/UserBoImpl.java
@@ -1,4 +1,34 @@
 package com.dev.pos.bo.impl;
 
-public class UserBoImpl {
+import com.dev.pos.Enum.DaoType;
+import com.dev.pos.bo.custom.UserBo;
+import com.dev.pos.dao.DaoFactory;
+import com.dev.pos.dao.custom.UserDao;
+import com.dev.pos.dao.impl.UserDaoImpl;
+import com.dev.pos.dto.UserDTO;
+import com.dev.pos.entity.User;
+
+public class UserBoImpl implements UserBo {
+
+    UserDao userDao = (UserDao) DaoFactory.getInstance().getDao(DaoType.USER);
+
+    @Override
+    public boolean saveUser(UserDTO userDTO) throws Exception {
+        return userDao.save(new User(
+                userDTO.getEmail(),
+                userDTO.getPassword()
+        ));
+    }
+
+    @Override
+    public UserDTO findUser(String email) throws Exception {
+        User user = userDao.find(email);
+        if (user != null) {
+            return new UserDTO(
+                    user.getEmail(),
+                    user.getPassword()
+            );
+        }
+        return null;
+    }
 }
