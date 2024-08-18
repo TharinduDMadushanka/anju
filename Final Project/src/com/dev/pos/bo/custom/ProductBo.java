2cc65a9ccd1dc12ab22b94a1ad88e540a257aed2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-18 10:51:21 +0530

    complete ProductBo interface

--- Final Project/src/com/dev/pos/bo/custom/ProductBo.java
+++ Final Project/src/com/dev/pos/bo/custom/ProductBo.java
@@ -1,4 +1,27 @@
 package com.dev.pos.bo.custom;
 
+import com.dev.pos.dto.ProductDto;
+import com.dev.pos.entity.Product;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ProductBo {
+
+    public boolean saveProduct(ProductDto productDto) throws Exception;
+
+    public boolean updateProduct(ProductDto productDto) throws Exception;
+
+    public boolean deleteProduct(int code) throws Exception;
+
+    public ProductDto findProduct(int code) throws Exception;
+
+    public List<ProductDto> findAllProduct() throws Exception;
+
+    public List<ProductDto> searchProduct(String value) throws Exception;
+
+    public int getLastProductId() throws Exception;
+
+    public List<ProductDto> searchByDescription(String value) throws Exception;
+
 }
--- Final Project/src/com/dev/pos/bo/impl/ProductBoImpl.java
+++ Final Project/src/com/dev/pos/bo/impl/ProductBoImpl.java
@@ -1,4 +1,54 @@
 package com.dev.pos.bo.impl;
 
-public class ProductBoImpl {
+import com.dev.pos.bo.custom.ProductBo;
+import com.dev.pos.dto.ProductDto;
+import com.dev.pos.entity.Product;
+
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+
+public class ProductBoImpl implements ProductBo {
+    @Override
+    public boolean saveProduct(ProductDto productDto) throws SQLException, ClassNotFoundException {
+        return productDao.save(new Product(
+                productDto.getCode(),
+                productDto.getDescription()
+        ));
+    }
+
+    @Override
+    public boolean updateProduct(ProductDto productDto) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteProduct(int code) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public ProductDto findProduct(int code) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> findAllProduct() throws SQLException, ClassNotFoundException {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ProductDto> searchProduct(String value) throws SQLException, ClassNotFoundException {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public int getLastProductId() throws SQLException, ClassNotFoundException {
+        return 0;
+    }
+
+    @Override
+    public List<ProductDto> searchByDescription(String value) throws SQLException, ClassNotFoundException {
+        return Collections.emptyList();
+    }
 }
