7598e71f6201e5f707e356c98200b00da52433bd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-22 10:10:10 +0530

    create NewBatch UI and generate components

--- Final Project/src/com/dev/pos/controller/NewBatchFormController.java
+++ Final Project/src/com/dev/pos/controller/NewBatchFormController.java
@@ -1,4 +1,56 @@
 package com.dev.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class NewBatchFormController {
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private AnchorPane context;
+
+    @FXML
+    private ToggleGroup discount;
+
+    @FXML
+    private ImageView imgQR;
+
+    @FXML
+    private RadioButton rdNo;
+
+    @FXML
+    private RadioButton rdYes;
+
+    @FXML
+    private TextField txtBuyingPrice;
+
+    @FXML
+    private TextArea txtDescription;
+
+    @FXML
+    private TextField txtProductCode;
+
+    @FXML
+    private TextField txtQTy;
+
+    @FXML
+    private TextField txtSellingPrice;
+
+    @FXML
+    private TextField txtShowPrice;
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
 }
