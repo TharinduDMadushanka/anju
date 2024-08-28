896d5f8cefb131231f5567ed1fdd4c1d2aee0787
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-28 09:04:26 +0530

    create BatchDaoImpl

---
+++ Final Project/src/com/dev/pos/dao/impl/BatchDaoImpl.java
@@ -0,0 +1,39 @@
+package com.dev.pos.dao.impl;
+
+import com.dev.pos.dao.custom.BatchDao;
+import com.dev.pos.entity.Batch;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BatchDaoImpl implements BatchDao {
+    @Override
+    public boolean save(Batch batch) throws Exception, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Batch batch) throws Exception, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String s) throws Exception, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public Batch find(String s) throws Exception, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public List<Batch> findAll() throws Exception, ClassNotFoundException {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Batch> search(String s) throws Exception, ClassNotFoundException {
+        return Collections.emptyList();
+    }
+}
