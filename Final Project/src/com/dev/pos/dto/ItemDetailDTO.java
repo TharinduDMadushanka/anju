5ec85b2bfa5ec5c4f827e2a37b3330e6357bcb94
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-09 21:00:36 +0530

    created ItemDetailDTO dto

--- Final Project/src/com/dev/pos/dto/ItemDetailDTO.java
+++ Final Project/src/com/dev/pos/dto/ItemDetailDTO.java
@@ -2,8 +2,50 @@ package com.dev.pos.dto;
 
 public class ItemDetailDTO {
 
-    String detailCode;
-    int qty;
+    private int detailCode;
+    private int qty;
+    private double discount;
+    private double amount;
 
+    public ItemDetailDTO() {
+    }
 
+    public ItemDetailDTO(int detailCode, int qty, double discount, double amount) {
+        this.detailCode = detailCode;
+        this.qty = qty;
+        this.discount = discount;
+        this.amount = amount;
+    }
+
+    public int getDetailCode() {
+        return detailCode;
+    }
+
+    public void setDetailCode(int detailCode) {
+        this.detailCode = detailCode;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
